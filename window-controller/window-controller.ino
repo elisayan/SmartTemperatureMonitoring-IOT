b56e607365dd037a3c7bbeeebb56362b9fbac38c
Elisa Yan <elisa.yan@studio.unibo.it>
2025-03-05 18:29:21 +0100

    refactored window controller

--- window-controller/window-controller.ino
+++ window-controller/window-controller.ino
@@ -1,20 +1,50 @@
 #include <LiquidCrystal_I2C.h>
-#include <Servo.h>
-#include "MsgService.h"
 #include "Scheduler.h"
+#include "ServoMotor.h"
 #include "Config.h"
+#include "MsgService.h"
+#include "MsgReceiverTask.h"
+#include "ButtonTask.h"
+#include "PotTask.h"
+#include "DisplayTask.h"
 
-Scheduler sched;
 LiquidCrystal_I2C lcd(0x27, 20, 4);
+ServoMotor windowServo(SERVO_PIN);
+
+bool manualMode = false;
+int windowPosition = 0;
+float currentTemperature = 0.0;
+
+Scheduler scheduler;
 
 void setup() {
   Serial.begin(115200);
   MsgService.init();
-  sched.init(100);
 
-  
+  lcd.init();
+  lcd.backlight();
+  windowServo.on();
+
+  scheduler.init(100);
+
+  Task* msgTask = new MsgReceiverTask(MsgService, manualMode, windowPosition, currentTemperature);
+  Task* btnTask = new ButtonTask(BUTTON_PIN, manualMode, MsgService);
+  Task* potTask = new PotTask(POT_PIN, windowPosition, manualMode, MsgService);
+  Task* dispTask = new DisplayTask(lcd, manualMode, windowPosition, currentTemperature);
+
+  scheduler.addTask(msgTask);
+  scheduler.addTask(btnTask);
+  scheduler.addTask(potTask);
+  scheduler.addTask(dispTask);
+
+  msgTask->init(100);
+  btnTask->init(50);
+  potTask->init(150);
+  dispTask->init(500);
 }
 
 void loop() {
-  sched.schedule();
+  scheduler.schedule();
+  windowServo.setPosition(windowPosition);
+  delay(10);
 }
