5ebf3eb551b2db6c9a2f2ab4258f3d00a527b450
Elisa Yan <elisa.yan@studio.unibo.it>
2025-03-30 18:46:54 +0200

    added plant in msg receiver task

--- window-controller/MsgReceiverTask.cpp
+++ window-controller/MsgReceiverTask.cpp
@@ -1,72 +1,97 @@
 #include "MsgReceiverTask.h"
 #include "Arduino.h"
 
-MsgReceiverTask::MsgReceiverTask(bool mode, int pos, float temp, LCDDisplayI2C* lcd) {
-  this->lcd = lcd;
-  this->manualMode = mode;
-  this->currentTemperature = temp;
-  this->windowPosition = pos;
+MsgReceiverTask::MsgReceiverTask(WindowControllerPlant* pPlant) {
+  this->pPlant = pPlant;
 }
 
 void MsgReceiverTask::tick() {
-  
-    while (Serial.available() > 0) {
-    Serial.read();  // Svuota il buffer
-  }
   if (MsgService.isMsgAvailable()) {
     Msg* msg = MsgService.receiveMsg();
     if (msg) {
-      String line = msg->getContent();
-      processLine(line);
-      lcd->updateData(manualMode, windowPosition, currentTemperature);
-      // char buffer[50];
-      // snprintf(buffer, sizeof(buffer), "RECEIVED: %s %d %.2f",
-      //          manualMode ? "MANUAL" : "AUTOMATIC", windowPosition, currentTemperature);
-      // Serial.println(buffer);
-      // Serial.println(currentTemperature);
-      //delete msg;
-    }
-
-    if (msg == nullptr) {
-      Serial.println("MsgService NULL!");
-      return;
+      processLine(msg->getContent());
+      delete msg;
     }
   }
-
-
 }
 
 void MsgReceiverTask::processLine(const String line) {
-  if (line.indexOf("TEMP:") != -1) {
-    currentTemperature = extractValue(line, "TEMP:").toFloat();
-  }
-
-  if (line.indexOf("POS:") != -1) {
-    windowPosition = extractValue(line, "POS:").toInt();
-  }
-
-  if (line.indexOf("MODE:") != -1) {
-    manualMode = extractValue(line, "MODE:") == "MANUAL";
+  if (line.startsWith("TEMP:")) {
+    pPlant->setCurrentTemperature(line.substring(5).toFloat());
+  } else if (line.startsWith("POS:")) {
+    pPlant->setWindowOpening(line.substring(4).toInt());
+  } else if (line.startsWith("MODE:")) {
+    if (line.substring(5) == "MANUAL") {
+      pPlant->switchToManualMode();
+    } else {
+      pPlant->switchToAutomaticMode();
+    }
   }
 }
 
-String MsgReceiverTask::extractValue(const String line, const String key) {
-  int keyIndex = line.indexOf(key);
-  if (keyIndex == -1) {
-    return "";
-  }
+// void MsgReceiverTask::tick() {
 
-  int colonIndex = line.indexOf(':', keyIndex);
-  if (colonIndex == -1) {
-    return "";
-  }
+//   while (Serial.available() > 0) {
+//     Serial.read();
+//   }
+//   if (MsgService.isMsgAvailable()) {
+//     Msg* msg = MsgService.receiveMsg();
+//     if (msg) {
+//       String line = msg->getContent();
+//       processLine(line);
 
-  int nextSpace = line.indexOf(' ', colonIndex);
-  if (nextSpace == -1) {
-    nextSpace = line.length();
-  }
+//       if (manualMode) {
+//         lcd->updateManualData(windowPosition, currentTemperature);
+//       } else {
+//         lcd->updateAutoData(windowPosition);
+//       }
 
-  String value = line.substring(colonIndex + 1, nextSpace);
-  value.trim();
-  return value;
-}
+//       // char buffer[50];
+//       // snprintf(buffer, sizeof(buffer), "RECEIVED: %s %d %.2f",
+//       //          manualMode ? "MANUAL" : "AUTOMATIC", windowPosition, currentTemperature);
+//       // Serial.println(buffer);
+//       // Serial.println(currentTemperature);
+//       //delete msg;
+//     }
+
+//     if (msg == nullptr) {
+//       Serial.println("MsgService NULL!");
+//       return;
+//     }
+//   }
+// }
+
+// void MsgReceiverTask::processLine(const String line) {
+//   if (line.indexOf("TEMP:") != -1) {
+//     currentTemperature = extractValue(line, "TEMP:").toFloat();
+//   }
+
+//   if (line.indexOf("POS:") != -1) {
+//     windowPosition = extractValue(line, "POS:").toInt();
+//   }
+
+//   if (line.indexOf("MODE:") != -1) {
+//     manualMode = extractValue(line, "MODE:") == "MANUAL";
+//   }
+// }
+
+// String MsgReceiverTask::extractValue(const String line, const String key) {
+//   int keyIndex = line.indexOf(key);
+//   if (keyIndex == -1) {
+//     return "";
+//   }
+
+//   int colonIndex = line.indexOf(':', keyIndex);
+//   if (colonIndex == -1) {
+//     return "";
+//   }
+
+//   int nextSpace = line.indexOf(' ', colonIndex);
+//   if (nextSpace == -1) {
+//     nextSpace = line.length();
+//   }
+
+//   String value = line.substring(colonIndex + 1, nextSpace);
+//   value.trim();
+//   return value;
+// }
--- window-controller/MsgReceiverTask.h
+++ window-controller/MsgReceiverTask.h
@@ -3,20 +3,15 @@
 
 #include "Task.h"
 #include "MsgService.h"
-#include "LCDDisplayI2C.h"
+#include "WindowControllerPlant.h"
 
 class MsgReceiverTask : public Task {
-  bool manualMode;
-  int windowPosition;
-  float currentTemperature;
-  LCDDisplayI2C* lcd;
+  WindowControllerPlant* pPlant;
 
   void processLine(const String line);
-  String extractValue(const String line, const String key);
-  void updateDisplay();
 
 public:
-  MsgReceiverTask(bool mode, int pos, float temp, LCDDisplayI2C* lcd);
+  MsgReceiverTask(WindowControllerPlant* pPlant);
   void tick();
 };
 
